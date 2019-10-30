1e8fbd2528c9f91d9c876917a794d6c3404e96a2
James Cramer <jcramer@simpleledger.io>
2019-10-30 14:32:21 -0400

    refactor unit tests
    and add "allow_inconclusive" handler.  This property was added to the
    unit test vectors to indicate that the validator may finish in an
    inconclusive state for reason such as "missing-txn".

--- test/validation.ts
+++ test/validation.ts
@@ -1,53 +1,72 @@
-import { ValidatorType1, GetRawTransactionAsync } from '../lib/validation';
-import {Crypto } from '../lib/crypto';
+import {Crypto } from "../lib/crypto";
+import { GetRawTransactionAsync, ValidatorType1 } from "../lib/validation";
 
-import * as assert from 'assert';
+import * as assert from "assert";
 import "mocha";
 
-const txUnitTestData: SlpValidityUnitTest[] = require('slp-unit-test-data/tx_input_tests.json');
+const txUnitTestData: SlpValidityUnitTest[] = require("slp-unit-test-data/tx_input_tests.json");
 
-describe('Slp', function() {
-    describe('isValidSlpTxid() -- SLP Transaction Validation Unit Tests', function() {
-        txUnitTestData.forEach(test => {
+describe("Slp", () => {
+    describe("isValidSlpTxid() -- SLP Transaction Validation Unit Tests", () => {
+        txUnitTestData.forEach((test) => {
             it(test.description, async () => {
 
-                // Create method for serving up the unit test transactions 
-                let getRawUnitTestTransaction: GetRawTransactionAsync = async (txid: string) => {
-                    let allTxns: SlpTestTxn[] = test.when.concat(test.should);
-                    let txn = allTxns.find(i => {
-                        let hash = Crypto.hash256(Buffer.from(i.tx, 'hex')).toString('hex');
-                        return hash === txid 
+                // Create method for serving up the unit test transactions
+                const getRawUnitTestTransaction: GetRawTransactionAsync = async (txid: string) => {
+                    const allTxns: SlpTestTxn[] = test.when.concat(test.should);
+                    const txn = allTxns.find((i) => {
+                        const hash = Crypto.hash256(Buffer.from(i.tx, "hex")).toString("hex");
+                        return hash === txid;
                     });
-                    if(txn)
+                    if (txn) {
                         return txn.tx;
-                    throw Error("Transaction data for the provided txid not found (txid: " + txid + ")")
-                }
-    
+                    }
+                    throw Error("Transaction data for the provided txid not found (txid: " + txid + ")");
+                };
+
                 // Create instance of Local Validator
-                var slpValidator = new ValidatorType1({ getRawTransaction: getRawUnitTestTransaction });
+                let slpValidator = new ValidatorType1({ getRawTransaction: getRawUnitTestTransaction });
 
                 // Pre-Load Validator the unit-test inputs
-                test.when.forEach(w => {
-                    slpValidator.addValidationFromStore(w.tx, w.valid)
+                test.when.forEach((w) => {
+                    slpValidator.addValidationFromStore(w.tx, w.valid);
                 });
 
-                let txid = Crypto.hash256(Buffer.from(test.should[0].tx, 'hex')).toString('hex');
-                let shouldBeValid = test.should[0].valid;
-                let isValid = await slpValidator.isValidSlpTxid({ txid });
-                if(isValid === false)
-                    console.log('invalid reason:', slpValidator.cachedValidations.get(txid)!.invalidReason);
+                // if (test.description === "When given two SLP-valid inputs, the SEND should be SLP-invalid since token version/type changed to NFT1 parent type (having fake child GENESIS txid)") {
+                //     console.log("bp");
+                // }
+
+                const txid = Crypto.hash256(Buffer.from(test.should[0].tx, "hex")).toString("hex");
+                const shouldBeValid = test.should[0].valid;
+                let isValid;
+                try {
+                    isValid = await slpValidator.isValidSlpTxid({ txid });
+                } catch (error) {
+                    if (error.message.includes("Transaction data for the provided txid not found") &&
+                        test.allow_inconclusive && test.inconclusive_reason === "missing-txn") {
+                            isValid = false;
+                    } else {
+                        throw error;
+                    }
+                }
+                if (isValid === false) {
+                    console.log("invalid reason:", slpValidator.cachedValidations.get(txid)!.invalidReason);
+                }
                 assert.equal(isValid, shouldBeValid);
             });
-        })
+        });
     });
 });
 
 interface SlpValidityUnitTest {
     description: string;
-    when: SlpTestTxn[]
-    should: SlpTestTxn[]
+    when: SlpTestTxn[];
+    should: SlpTestTxn[];
+    allow_inconclusive: boolean;
+    inconclusive_reason: string;
 }
 
 interface SlpTestTxn {
-    tx: string, valid: boolean
+    tx: string;
+    valid: boolean;
 }
